825d05607fe64d1feff19ea1cd6240db971c2b4b
griselangelina <34536166+griselangelina@users.noreply.github.com>
2022-03-15 22:24:32 +0700

    update validate function

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,31 +1,31 @@
-export const maxLength = (text, length) => {
-  return text.replace(/ /g, '').length > length ? `Nama terlalu panjang` : null;
+export const maxLength = (name,text, length) => {
+  return text.replace(/ /g, '').length > length ? `${name} terlalu panjang` : null;
 };
 
-export const minLength = (text, length) => {
-  return text.replace(/ /g, '').length < length ? `Nama terlalu pendek` : null;
+export const minLength = (name,text, length) => {
+  return text.replace(/ /g, '').length < length ? `${name} terlalu pendek` : null;
 };
 
-export const lengthBetween = (text, val1, val2) => {
+export const lengthBetween = (name,text, val1, val2) => {
   return text.replace(/ /g, '').length < val1 || text.replace(/ /g, '').length > val2
-    ? `Kata sandi harus mengandung ${val1}-${val2} karakter`
+    ? `${name} harus mengandung ${val1}-${val2} karakter`
     : null;
 };
 
-export const phoneNumber = (number) => {
+export const phoneNumber = (name,number) => {
   const isPhone = /^[0-9]*$/i.test(number);
 
   return number.length > 14
-      ? `Nomor telepon tidak valid`
+      ? `${name} tidak valid`
       : !isPhone
-      ? `Nomor telepon tidak valid`
+      ? `${name} tidak valid`
       : null
 };
 
-export const passwordFormat = (text) => {
+export const passwordFormat = (name, text) => {
   if(text.length > 0){
   return /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])/.test(text)
     ? null
-    : `Kata sandi kombinasi huruf besar, huruf kecil, dan angka`;
+    : `${name} kombinasi huruf besar, huruf kecil, dan angka`;
   }else return null;
 };
